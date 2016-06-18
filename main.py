cc688ea91992edb5a6b90437afa07bc9e10f026b
TomJamesGray <tomg77@yahoo.co.uk>
2016-06-18 19:38:03 +0100

    Added strip list method to clear up input from HLTV

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from lxml import html
 import requests
 import sys
 import logging
+def stripList(data):
+    for i in range(0,len(data)):
+        data[i] = str(data[i]).strip()
+
+    return data
 def main():
     logging.basicConfig(level=logging.DEBUG)
 
@@ -14,10 +19,11 @@ def main():
     if not page.status_code == 200:
         sys.exit(1)
     tree = html.fromstring(page.content)
-    teamOne = tree.xpath("///div[@class='matchTeam1Cell']/a/text()")
-    teamTwo = tree.xpath("///div[@class='matchTeam2Cell']/a/text()")
-
+    #Get the data from the page
+    teamOne = stripList(tree.xpath("///div[@class='matchTeam1Cell']/a/text()"))
+    teamTwo = stripList(tree.xpath("///div[@class='matchTeam2Cell']/a/text()"))
     print(teamOne)
     print(teamTwo)
+
 if __name__ == "__main__":
     main()
