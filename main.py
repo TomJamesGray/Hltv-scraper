9af4202094626daedcc17ee86de863db6214593e
TomJamesGray <tomg77@yahoo.co.uk>
2016-06-20 17:33:57 +0100

    Retrieve matchURL

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import sys
 import logging
 import sqlite3
+baseURL = "http://www.hltv.org/"
 def stripList(data):
     for i in range(0,len(data)):
         data[i] = str(data[i]).strip()
@@ -11,6 +12,12 @@ def stripList(data):
     #Remove blanks from list
     data = list(filter(None,data))
     return data
+
+#Strip a string from all elements in a list
+def stripFromList(data,toStrip):
+    for i in range(0,len(data)):
+        data[i] = data[i].strip(toStrip)
+
 def insertMatchData(teamOne,teamTwo,db):
     conn = sqlite3.connect(db)
     curs = conn.cursor()
@@ -22,11 +29,13 @@ def insertMatchData(teamOne,teamTwo,db):
         curs.execute("INSERT INTO matches ('Team1','Team2') VALUES (?,?)",(teamOne[i],teamTwo[i]))
 
     conn.commit()
+
 def main():
+    global baseURL
     logging.basicConfig(level=logging.DEBUG)
 
     #Get raw match page from hltv
-    page = requests.get('http://www.hltv.org/matches/')
+    page = requests.get(baseURL + "matches/")
     logging.info("Code {} from hltv request".format(page.status_code))
 
     #Check status code - 200 is good
@@ -36,7 +45,10 @@ def main():
     #Get the data from the page
     teamOne = stripList(tree.xpath("///div[@class='matchTeam1Cell']/a/text()"))
     teamTwo = stripList(tree.xpath("///div[@class='matchTeam2Cell']/a/child::text()"))
-    print("Length teamOne {} \nLengthTeamTwo {}".format(len(teamOne),len(teamTwo)))
+    #Get match url
+    matchUrl = stripList(tree.xpath("///div[@class='matchActionCell']/a/@href"))
+    print(matchUrl)
+#    print("Length teamOne {} \nLengthTeamTwo {}".format(len(teamOne),len(teamTwo)))
     print("Inserting into db")
     insertMatchData(teamOne,teamTwo,"matches.db")
 if __name__ == "__main__":
