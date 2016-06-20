e7cd74edeb42c054ef6554a73cebb15e4a61e111
TomJamesGray <tomg77@yahoo.co.uk>
2016-06-20 17:12:40 +0100

    Inserting team data to db

--- main.py
+++ main.py
@@ -3,11 +3,25 @@ from lxml import html
 import requests
 import sys
 import logging
+import sqlite3
 def stripList(data):
     for i in range(0,len(data)):
         data[i] = str(data[i]).strip()
-
+   
+    #Remove blanks from list
+    data = list(filter(None,data))
     return data
+def insertMatchData(teamOne,teamTwo,db):
+    conn = sqlite3.connect(db)
+    curs = conn.cursor()
+    if not len(teamOne) == len(teamTwo):
+        raise ValueError("Amount of teams in matches aren't equal")
+        sys.exit(1)
+
+    for i in range(0,len(teamOne)):
+        curs.execute("INSERT INTO matches ('Team1','Team2') VALUES (?,?)",(teamOne[i],teamTwo[i]))
+
+    conn.commit()
 def main():
     logging.basicConfig(level=logging.DEBUG)
 
@@ -21,9 +35,9 @@ def main():
     tree = html.fromstring(page.content)
     #Get the data from the page
     teamOne = stripList(tree.xpath("///div[@class='matchTeam1Cell']/a/text()"))
-    teamTwo = stripList(tree.xpath("///div[@class='matchTeam2Cell']/a/text()"))
-    print(teamOne)
-    print(teamTwo)
-
+    teamTwo = stripList(tree.xpath("///div[@class='matchTeam2Cell']/a/child::text()"))
+    print("Length teamOne {} \nLengthTeamTwo {}".format(len(teamOne),len(teamTwo)))
+    print("Inserting into db")
+    insertMatchData(teamOne,teamTwo,"matches.db")
 if __name__ == "__main__":
     main()
